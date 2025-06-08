16f6b669772e9a4f1f213000da58fa157f863216
Luke Oladejo <bylukeo@gmail.com>
2025-06-08 11:09:05 +0100

    upload project structure files

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
+      <h1>Welcome to the ALX Project!</h1>
+      <p>This is the Next.js starter page.</p>
+    </main>
+  );
+}
