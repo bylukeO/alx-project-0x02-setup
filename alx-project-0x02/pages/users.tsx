7f4caefbd985e91390e85a76ca26243ed357eabe
Luke Oladejo <bylukeo@gmail.com>
2025-06-08 12:40:36 +0100

    update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { Address, User } from "../../interfaces";
 
-export default function Users() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(true);
+interface UsersProps {
+  users: User[];
+}
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-        setLoading(false);
-      });
-  }, []);
-
+export default function Users({ users = [] }: UsersProps) {
   return (
     <>
       <Header />
       <main style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
         <h1>Users Page</h1>
         <p>This page displays a list of users.</p>
-        {loading ? (
-          <p>Loading...</p>
+        {users.length === 0 ? (
+          <p>No users found.</p>
         ) : (
           users.map((user) => (
             <UserCard
@@ -37,4 +29,15 @@ export default function Users() {
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
 }
