a7a5083e31431c98bc9a04f65cf48e7457011c6d
Luke Oladejo <bylukeo@gmail.com>
2025-06-08 11:11:14 +0100

    update header.tsx file

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header: React.FC = () => (
+  <header style={{ width: "100%", padding: "1rem 0", background: "#f5f5f5", textAlign: "center" }}>
+    <h2>ALX Project Header</h2>
+  </header>
+);
+
+export default Header;
