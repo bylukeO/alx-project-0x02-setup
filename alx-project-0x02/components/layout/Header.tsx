01cdc95e10d9eb9e2a7ab9ca2b529bdedfd47ee4
Luke Oladejo <bylukeo@gmail.com>
2025-06-08 12:08:19 +0100

    Implement navigation between pages using a reusable Header component.

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 const Header: React.FC = () => (
   <header style={{ width: "100%", padding: "1rem 0", background: "#000000", textAlign: "center" }}>
-    <h2>ALX Project Header</h2>
+    <h2 style={{ color: "#fff" }}>ALX Project Header</h2>
     <nav>
-      <Link href="/home" style={{ margin: "0 1rem" }}>Home</Link>
-      <Link href="/about" style={{ margin: "0 1rem" }}>About</Link>
+      <Link href="/home" style={{ margin: "0 1rem", color: "#fff", textDecoration: "none" }}>Home</Link>
+      <Link href="/about" style={{ margin: "0 1rem", color: "#fff", textDecoration: "none" }}>About</Link>
+      <Link href="/posts" style={{ margin: "0 1rem", color: "#fff", textDecoration: "none" }}>Posts</Link>
     </nav>
   </header>
 );
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/layout/Header";
+
+export default function Posts() {
+  return (
+    <>
+      <Header />
+      <main style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
+        <h1>Posts Page</h1>
+        <p>This page will display all posts.</p>
+      </main>
+    </>
+  );
+}
